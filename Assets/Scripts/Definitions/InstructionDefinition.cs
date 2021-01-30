85a553f0c0d6b1243486ec2458cc45db70a3e120
piranhaMagi <piranhaMagi@gmail.com>
2021-01-30 10:07:17 -0800

    create instruction definition

---
+++ Assets/Scripts/Definitions/InstructionDefinition.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * Defines an enemy type
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "New Instruction", menuName = "Instruction")]
+public class InstructionDefinition : ScriptableObject
+{
+    public Enums.Ingredient correctIngredient;
+    public string instruction;
+}
