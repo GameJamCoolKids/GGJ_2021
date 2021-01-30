2e3ddaa53bdf9dd24aacace70026070def197d95
piranhaMagi <piranhaMagi@gmail.com>
2021-01-30 12:20:45 -0800

    put in basic gamecontroller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,15 +5,41 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public List<LevelDefinition> levels;
+    public LevelDefinition currentLevel;
+    public int attempts; // the number of incorrect ingredient use attempts before game over
 
-    void Start()
+    private void Start()
     {
-        
+        foreach (LevelDefinition level in levels)
+        {
+            currentLevel = level;
+            StartLevel(currentLevel);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+    }
+
+    // sets the win and loss conditions for the level
+    public void StartLevel(LevelDefinition level)
+    {
+
+    }
+
+    // current level that is active in the scene, can get currentLevel properties such as instructions, etc
+    public LevelDefinition GetCurrentLevel()
+    {
+        return currentLevel;
+    }
+
+
+    // PLACEHOLDERS UNTIL WE GET THEM IMPLEMENTED IN OTHER SCRIPTS
+
+    // the script that deals with dropping ingredients into the pot will tell us whether the choice was correct
+    // this uses a method from that script
+    public bool IngredientChoiceIsIncorrect()
+    {
+        return false;
     }
 }
