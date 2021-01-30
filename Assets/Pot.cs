0583673de8a5e9896f5f920418208daf46df4bd5
Alexander Scaini <alexanderscaini@gmail.com>
2021-01-30 17:37:49 -0500

    mouse can drag things but physics dont stop. also added colliders and a trigger to the pot

---
+++ Assets/Pot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("something went in");
+    }
+
+}
