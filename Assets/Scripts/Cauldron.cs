866373ba87c61bf379e2cf4c37add352b940e67f
piranhaMagi <piranhaMagi@gmail.com>
2021-01-30 16:46:23 -0800

    submit basics for caudlron script

---
+++ Assets/Scripts/Cauldron.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Logic for checking whether ingredient is correct or not
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cauldron : MonoBehaviour
+{
+    public GameController gameController;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        IngredientController ingredientController = collision.gameObject.GetComponent<IngredientController>();
+        if (ingredientController.ingredient == Enums.Ingredient.BadEgg)
+        {
+
+        }
+    }
+}
