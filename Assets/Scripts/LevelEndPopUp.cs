994e48bbca0f57a69011d54debfa80fc8c80f553
piranhaMagi <piranhaMagi@gmail.com>
2021-01-30 22:53:06 -0800

    add lose button hook up to restart level

--- Assets/Scripts/LevelEndPopUp.cs
+++ Assets/Scripts/LevelEndPopUp.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelEndPopUp : MonoBehaviour
 {
     public GameController gameController;
     public TextMeshProUGUI levelEndHeaderTmp;
     public TextMeshProUGUI levelEndButtonTmp;
+    public Button button;
+
+    private void Start()
+    {
+        button.onClick.AddListener(delegate {
+            gameController.StartLevel(gameController.GetCurrentLevel()); });
+    }
 }
