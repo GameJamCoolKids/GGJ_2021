62f8acd3c7b15b09225798fb4ef261c3b1a96384
piranhaMagi <piranhaMagi@gmail.com>
2021-01-30 16:16:49 -0800

    add audiocontroller

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource LobbyMusic;
+    public AudioSource MandrakeSound;
+
+    private List<AudioSource> _soundEffects;
+    private List<AudioSource> _music;
+    
+    public void Start()
+    {
+        _soundEffects = new List<AudioSource>()
+        {
+           MandrakeSound,
+        };
+
+        _music = new List<AudioSource>()
+        {
+            LobbyMusic,
+        };
+    }
+}
